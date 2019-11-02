f28cc61c48e13231c11fcda19befa417c3e2fa4e
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2019-11-02 20:41:02 +0000

    Page Controller setup

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,4 +1,12 @@
 <?php
+# @Author: John Carlo M. Ramos
+# @Date:   2019-11-02T20:17:59+00:00
+# @Email:  !!!!!---CTRL + ALT + C = Colour Picker---!!!!!
+# @Last modified by:   John Carlo M. Ramos
+# @Last modified time: 2019-11-02T20:39:58+00:00
+
+
+
 
 namespace App\Http\Controllers;
 
@@ -6,5 +14,11 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+  public function welcome() {
+    return view('welcome');
+  }
+
+  public function about() {
+    return view('about');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 # @Date:   2019-11-02T14:04:58+00:00
 # @Email:  !!!!!---CTRL + ALT + C = Colour Picker---!!!!!
 # @Last modified by:   John Carlo M. Ramos
-# @Last modified time: 2019-11-02T17:03:13+00:00
+# @Last modified time: 2019-11-02T20:38:46+00:00
 
 
 
@@ -19,13 +19,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'PageController@welcome')->name('welcome');
+Route::get('/about', 'PageController@about')->name('about');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+// From Auth V1:
 Route::get('/admin/home', 'Admin\HomeController@index')->name('admin.home');
 Route::get('/user/home', 'User\HomeController@index')->name('user.home');
+
+// From CRUD V1.5:
+// Route::get('/welcome', 'PageController@welcome')->name('welcome');
