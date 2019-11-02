042bdbb5cbc6c862cd802d0eefc116703e59531d
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2019-11-02 21:11:08 +0000

    Routes setup and Books Table Migration

---
+++ database/migrations/2019_11_02_210502_create_books_table.php
@@ -0,0 +1,45 @@
+<?php
+# @Author: John Carlo M. Ramos
+# @Date:   2019-11-02T21:05:02+00:00
+# @Email:  !!!!!---CTRL + ALT + C = Colour Picker---!!!!!
+# @Last modified by:   John Carlo M. Ramos
+# @Last modified time: 2019-11-02T21:08:33+00:00
+
+
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('author');
+            $table->string('publisher');
+            $table->integer('year')->unsigned();
+            $table->string('isbn')->unique();
+            $table->decimal('price', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 # @Date:   2019-11-02T14:04:58+00:00
 # @Email:  !!!!!---CTRL + ALT + C = Colour Picker---!!!!!
 # @Last modified by:   John Carlo M. Ramos
-# @Last modified time: 2019-11-02T20:38:46+00:00
+# @Last modified time: 2019-11-02T21:04:15+00:00
 
 
 
@@ -36,3 +36,13 @@ Route::get('/user/home', 'User\HomeController@index')->name('user.home');
 
 // From CRUD V1.5:
 // Route::get('/welcome', 'PageController@welcome')->name('welcome');
+Route::get('/admin/books', 'Admin\BookController@index')->name('admin.books.index');
+Route::get('/admin/books/create', 'Admin\BookController@create')->name('admin.books.create');
+Route::get('/admin/books/{id}', 'Admin\BookController@show')->name('admin.books.show');
+Route::post('/admin/books/store', 'Admin\BookController@store')->name('admin.books.store');
+Route::get('/admin/books/{id}/edit', 'Admin\BookController@edit')->name('admin.books.edit');
+Route::put('/admin/books/{id}', 'Admin\BookController@update')->name('admin.books.update');
+Route::delete('/admin/books/{id}', 'Admin\BookController@destroy')->name('admin.books.destroy');
+
+Route::get('/user/books', 'User\BookController@index')->name('user.books.index');
+Route::get('/user/books/{id}', 'User\BookController@show')->name('user.books.show');
